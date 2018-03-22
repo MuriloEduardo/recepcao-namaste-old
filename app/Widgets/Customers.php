95cec8d2cfe46c0172eec4fd9e5a3c9211e45386
˜MuriloEduardo˜ <muriloeduardoooooo@gmail.com>
2018-03-22 15:50:06 -0300

    Deploy in heroku

---
+++ app/Widgets/Customers.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Widgets;
+
+use Arrilot\Widgets\AbstractWidget;
+use TCG\Voyager\Facades\Voyager;
+
+class Customers extends AbstractWidget
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Customer::count();
+        $string = 'Cliente';
+
+        if ($count > 1) {
+            $string = $string . 's';
+        }
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-people',
+            'title'  => "{$count} {$string}",
+            'text'   => "Você tem {$count} {$string}. Clique no botão abaixo para ver todos.",
+            'button' => [
+                'text' => 'Ver todos clientes',
+                'link' => '/admin/clientes',
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+}

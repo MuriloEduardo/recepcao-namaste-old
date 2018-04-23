b200ca0d1f136aff8889ee0574ec7807e8efdaa2
˜MuriloEduardo˜ <muriloeduardoooooo@gmail.com>
2018-04-23 14:08:16 -0300

    concertando versionamento

--- resources/views/vendor/voyager/participacoes/form.blade.php
+++ resources/views/vendor/voyager/participacoes/form.blade.php
@@ -61,11 +61,11 @@
 </form>
 
 <iframe id="form_target" name="form_target" style="display:none"></iframe>
-<form id="my_form" action="{{ route('voyager.upload') }}" target="form_target" method="post"
+<form id="participations_form" action="{{ route('voyager.upload') }}" target="form_target" method="post"
         enctype="multipart/form-data" style="width:0;height:0;overflow:hidden">
-    <input name="image" id="upload_file" type="file"
-                onchange="$('#my_form').submit();this.value='';">
-    <input type="hidden" name="type_slug" id="type_slug" value="{{ $dataType->slug }}">
+    <input name="image" id="participations_upload_file" type="file"
+                onchange="$('#participations_form').submit();this.value='';">
+    <input type="hidden" name="type_slug" id="participations_type_slug" value="{{ $dataType->slug }}">
     {{ csrf_field() }}
 </form>
 
